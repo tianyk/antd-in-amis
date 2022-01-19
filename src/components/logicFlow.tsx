551de1d4875baa62f455c725df30dad42f672184
yongketian@gmail.com <tyk>
2022-01-19 18:19:37 +0800

    logicflow

--- src/components/logicFlow.tsx
+++ src/components/logicFlow.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { Renderer, classnames } from 'amis';
+import autoBindReact from 'auto-bind/react';
+import { FormItem, classnames } from 'amis';
+
 import LogicFlow from '@logicflow/core'
 import { CurvedEdge } from '@logicflow/extension';
 
-import type { RendererProps } from 'amis/lib/factory';
+import type { FormControlProps } from 'amis/lib/renderers/Form/Item';
 import type { GraphConfigData } from '@logicflow/core';
 import type { Definition as LogicFlowOptions } from '@logicflow/core/types/options';
 
@@ -12,10 +14,14 @@ import './logicFlow.less';
 
 LogicFlow.use(CurvedEdge);
 
-interface LogicFlowProps extends RendererProps {
+type Listener = () => void;
+
+interface LogicFlowProps extends FormControlProps {
+    value: GraphConfigData;
     options: {
         config?: LogicFlowOptions,
-        data?: GraphConfigData
+
+        listeners?: Record<string, Listener>
     }
 }
 
@@ -23,7 +29,7 @@ interface LogicFlowState {
     lf?: LogicFlow
 }
 
-@Renderer({
+@FormItem({
     type: 'my-logic-flow'
 })
 export default class extends React.Component<LogicFlowProps, LogicFlowState> {
@@ -32,18 +38,66 @@ export default class extends React.Component<LogicFlowProps, LogicFlowState> {
     constructor(props: LogicFlowProps) {
         super(props);
         this.lfRef = React.createRef<HTMLDivElement>();
+        autoBindReact(this);
+    }
+
+    onChange() {
+        console.log('change')
+        const lf = this.state.lf;
+        const graphData = lf?.getGraphData();
+        this.props.onChange(graphData);
     }
 
     componentDidMount() {
-        // console.log(this.props.options.config?.grid)
+        console.log('componentDidMount');
         const logicflow = new LogicFlow({
-            ...this.props.options.config,
+            ...this.props.options?.config,
             container: this.lfRef.current!
         });
-        logicflow.render(this.props.options.data);
+        // 绑定事件
+        if (this.props.options?.listeners) {
+            // 绑定事件
+            for (let eventName in this.props.options.listeners) {
+                const listener = this.props.options.listeners[eventName];
+                logicflow.on(eventName, function (...args) {
+                    listener.apply(this, [...args, logicflow]);
+                });
+            }
+        }
+        // @see http://logic-flow.org/api/eventCenterApi.html#%E8%8A%82%E7%82%B9%E4%BA%8B%E4%BB%B6
+        [
+            'node:delete',
+            'node:add',
+            'node:dnd-add',
+            'node:dnd-drag',
+            'node:dragstart',
+            'node:drag',
+            'node:drop',
+
+            'edge:add',
+            'edge:delete',
+            'edge:adjust',
+            'edge:exchange-node',
+            'connection:not-allowed',
+
+            'text:update'
+        ].forEach((eventName) => {
+            logicflow.on(eventName, this.onChange);
+        });
+
+        // 初始化数据
+        logicflow.render(this.props.value);
         this.setState({
             lf: logicflow
         });
+    }
+
+    componentDidUpdate(prevProps: LogicFlowProps) {
+        if (this.props.value !== prevProps.value) {
+            const lf = this.state.lf;
+            lf?.clearData();
+            lf?.render(this.props.value);
+        }
     }
 
     render() {
